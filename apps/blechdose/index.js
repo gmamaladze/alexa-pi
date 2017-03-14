0338360521cb9219de15b46e608ed7c5a65a514e
George Mamaladze <gmamaladze@macbook-pro.speedport_w_921v_1_43_000>
2017-03-14 23:48:53 +0100

    Added simple driving

--- apps/blechdose/index.js
+++ apps/blechdose/index.js
@@ -1,19 +1,31 @@
 var alexa = require('alexa-app');
 
-// Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
 
-// Define an alexa-app
-var app = new alexa.app('blechdose');
+const appName = 'blechdose';
+var app = new alexa.app(appName);
 app.launch(function(req, res) {
-  res.say("Hello World!!");
+    res.say("Was soll ich tun? Sage, zum Beispiel, fahre drei schritte nach vorne, oder drehe dich nach links um.");
+    console.log('%s: Launched.', appName);
 });
 
-app.intent('Hallo', {
-  "slots": { },
-  "utterances": ["Eins"]
-}, function(req, res) {
-  res.say('Zwei');
-});
+app.intent("DriveIntent",
+    function(req, res) {
+        var direction = req.slot('direction');
+        if (direction) {
+            if (!res.session('path')) {
+                res.session('path', []);
+            }
+            var path = res.session('path');
+            path.push(direction);
+            res.session('path', path);
+            res.say('Ok. Weiter?');
+            console.log('%s: %s', appName, path);
+        } else {
+            res.say("Was meinst du?");
+        }
+        res.shouldEndSession(false);
+    }
+  );
 
 module.exports = app;
